5ef8312fc7b89c85d7f4ac23214712eaad855e1f
lingeswrn <lingeswrn@gmail.com>
2017-02-14 19:31:14 +0530

    Pages

---
+++ platforms/browser/www/controllers/app.js
@@ -0,0 +1,7 @@
+var app = 	angular.module('BlankApp', ['ngMaterial','ngAnimate','ngAria','ngMessages']);
+
+/* LOGIN CONTROLLER */
+
+app.controller( 'loginCtrl' , function( $scope, $log, $http ){
+	
+});
