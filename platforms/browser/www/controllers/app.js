03a7ac8f4877fc5fa8a1b655181741697f6d5d2e
lingeswrn <lingeswrn@gmail.com>
2017-02-14 23:12:13 +0530

    Registration API

--- platforms/browser/www/controllers/app.js
+++ platforms/browser/www/controllers/app.js
@@ -1,7 +1,33 @@
 var app = 	angular.module('BlankApp', ['ngMaterial','ngAnimate','ngAria','ngMessages']);
 
-/* LOGIN CONTROLLER */
+/* CONSTANTS */
+app.service( 'Constants', function(){
+	this.apiUrl = function(){
+		return "http://budjet.000webhostapp.com/API/";
+	};
+});
 
+/* LOGIN CONTROLLER */
 app.controller( 'loginCtrl' , function( $scope, $log, $http ){
 	
+});
+
+/* REGISTRATION CONTROLLER */
+app.controller( 'registrationCtrl' , function( $scope, $log, $http, $mdToast, Constants ){
+	var API_URL = Constants.apiUrl();
+	$mdToast.show($mdToast.simple()
+  .textContent('hello!')
+  .parent(document.querySelectorAll('#toaster'))
+  .position('top left')
+  .hideDelay( 3000 )
+  .action('x'));
+	$scope.registrationFormSubmit = function( registration ){		
+		$http.post( API_URL + 'registration.php',{ data: registration} ).success( function( response ){
+			$log.log( response );
+			$mdToast.show({
+			  hideDelay   : 3000,
+			  position    : 'top right'
+			});
+		});
+	};
 });
