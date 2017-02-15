b49ae78b3ac13555e41bb1905a63a9b489838380
lingeswrn <lingeswrn@gmail.com>
2017-02-15 13:05:40 +0530

    registration toast

--- www/controllers/app.js
+++ www/controllers/app.js
@@ -7,27 +7,40 @@ app.service( 'Constants', function(){
 	};
 });
 
+/* TOAST */
+app.factory( 'Toast', function( $mdToast ){
+	var toast = {};
+	
+	toast.common = function( msg, className){
+		$mdToast.show($mdToast.simple()
+			.textContent( msg )
+			//.parent(document.querySelectorAll('#toaster'))
+			.position('top left')
+			.theme( className + '-toast' )
+			.hideDelay( 4000 )
+			.action('x'));
+	};
+	
+	return toast;
+});
+
 /* LOGIN CONTROLLER */
 app.controller( 'loginCtrl' , function( $scope, $log, $http ){
 	
 });
 
 /* REGISTRATION CONTROLLER */
-app.controller( 'registrationCtrl' , function( $scope, $log, $http, $mdToast, Constants ){
+app.controller( 'registrationCtrl' , function( $scope, $log, $http, $mdToast, Constants, Toast ){
 	var API_URL = Constants.apiUrl();
-	$mdToast.show($mdToast.simple()
-  .textContent('hello!')
-  .parent(document.querySelectorAll('#toaster'))
-  .position('top left')
-  .hideDelay( 3000 )
-  .action('x'));
+	
 	$scope.registrationFormSubmit = function( registration ){		
 		$http.post( API_URL + 'registration.php',{ data: registration} ).success( function( response ){
-			$log.log( response );
-			$mdToast.show({
-			  hideDelay   : 3000,
-			  position    : 'top right'
-			});
+			if( response.code == 400 ){
+				Toast.common( response.message, 'error' );
+			}else if( response.code == 200 ){
+				$scope.registration.name = "";
+				Toast.common( response.message, 'success' );
+			}
 		});
 	};
 });
