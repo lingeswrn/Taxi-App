b7fd891373b929e0bc3ae69582faee8faae886d4
lingeswrn <lingeswrn@gmail.com>
2017-02-19 18:47:06 +0530

    profile rides

--- platforms/browser/www/controllers/app.js
+++ platforms/browser/www/controllers/app.js
@@ -1,153 +1,191 @@
-var app = 	angular.module('BlankApp', ['ngMaterial','ngAnimate','ngAria','ngMessages','ngAutocomplete']);
+var app = 	angular.module('BlankApp', ['ngMessages','toastr','ngAnimate','google.places']);
 
 /* CONSTANTS */
 app.service( 'Constants', function(){
 	this.apiUrl = function(){
-		return "http://budjet.000webhostapp.com/API/";
-		//return "http://localhost/taxi_api/API/";
+		//return "http://budjet.000webhostapp.com/API/";
+		return "http://localhost/taxi/PHP/API/";
+	};
+	
+	this.userId = function(){
+		return angular.fromJson(sessionStorage.getItem( 'userData' )).id;
 	};
 });
 
-/*GOOGLE TAB DIRECTIVE */
-app.directive('disableTap', function($timeout) {
-  return {
-    link: function() {
-      $timeout(function() {
-        // Find google places div
-        _.findIndex(angular.element(document.querySelectorAll('.pac-container')), function(container) {
-          // disable ionic data tab
-          container.setAttribute('data-tap-disabled', 'true');
-          // leave input field if google-address-entry is selected
-          container.onclick = function( e ) {
-			  console.log(e)
-			  console.log(e.srcElement.parentNode.textContent)
-            document.getElementById('autocomplete').blur();
-			
-          };
-        });
-      },500);
-    }
-  };
+
+app.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }            
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
 });
 
-
-/* TOAST */
-app.factory( 'Toast', function( $mdToast ){
-	var toast = {};
-	
-	toast.common = function( msg, className){
-		$mdToast.show($mdToast.simple()
-			.textContent( msg )
-			//.parent(document.querySelectorAll('#toaster'))
-			.position('top left')
-			.theme( className + '-toast' )
-			.hideDelay( 4000 )
-			.action('x'));
-	};
-	
-	return toast;
+app.config(function(toastrConfig) {
+  angular.extend(toastrConfig, {
+    autoDismiss: true,
+    containerId: 'toast-container',
+    maxOpened: 0,    
+    newestOnTop: true,
+    positionClass: 'toast-top-center',
+    preventDuplicates: false,
+    preventOpenDuplicates: false,
+    target: 'body'
+  });
 });
 
 /* LOGIN CONTROLLER */
-app.controller( 'loginCtrl' , function( $scope, $log, $http, $mdToast, Constants, Toast ){
+app.controller( 'loginCtrl' , function( $scope, $log, $http, Constants, toastr){
 	var API_URL = Constants.apiUrl();
-	
 	$scope.loginFormsubmit = function( login ){		
 		$http.post( API_URL + 'login.php',{ data: login} ).success( function( response ){
 			if( response.code == 400 ){
-				Toast.common( response.message, 'error' );
+				toastr.error( response.message );
 			}else if( response.code == 200 ){
 				sessionStorage.setItem( 'userData',  JSON.stringify(response.data) );
 				location.href = "booking.html";
-				console.log( sessionStorage.getItem('userData') )
 			}
 		});
 	};
 });
 
 /* REGISTRATION CONTROLLER */
-app.controller( 'registrationCtrl' , function( $scope, $log, $http, $mdToast, Constants, Toast ){
+app.controller( 'registrationCtrl' , function( $scope, $log, $http, Constants, toastr ){
 	var API_URL = Constants.apiUrl();
 	
 	$scope.registrationFormSubmit = function( registration ){		
 		$http.post( API_URL + 'registration.php',{ data: registration} ).success( function( response ){
 			if( response.code == 400 ){
-				Toast.common( response.message, 'error' );
+				toastr.error( response.message );
 			}else if( response.code == 200 ){
-				Toast.common( response.message, 'success' );
+				//Toast.common( response.message, 'success' );
 			}
 		});
 	};
 });
 
 /* BOOKING CONTROLLER */
-app.controller( 'bookingCtrl' , function( $scope, $log, $http, $mdToast, Constants, Toast ){
-	/* var placeSearch, autocomplete;
-      var componentForm = {
-        street_number: 'short_name',
-        route: 'long_name',
-        locality: 'long_name',
-        administrative_area_level_1: 'short_name',
-        country: 'long_name',
-        postal_code: 'short_name'
-      };
+app.controller( 'bookingCtrl' , function( $scope, $log, $http, Constants ){
+	$scope.places = null;
+	$scope.bookingData = {};
+	var API_URL = Constants.apiUrl();
+	var USER_ID = Constants.userId();
+	
+	$("#basic_example_1").datetimepicker({format: 'dd-mm-yyyy hh:ii', autoclose: true, startDate:new Date()});
+	
+	$scope.autocompleteOptions = {
+		componentRestrictions: { country: 'in' },
+		types: ['geocode']
+	}
+	
+	$scope.bookingFormsubmit = function( bookings ){
+		
+		//*********DISTANCE AND DURATION**********************//
+		var service = new google.maps.DistanceMatrixService();
+		service.getDistanceMatrix({
+			origins: [bookings.from.formatted_address],
+			destinations: [bookings.to.formatted_address],
+			travelMode: google.maps.TravelMode.DRIVING,
+			unitSystem: google.maps.UnitSystem.METRIC,
+			avoidHighways: false,
+			avoidTolls: false
+		}, function (response, status) {
+			if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
+				var distance = response.rows[0].elements[0].distance.value / 1000;
+				var duration = response.rows[0].elements[0].duration.text;
+				$scope.booking.distance = distance.toFixed(1);
+				$scope.booking.duration = duration;				
+				$scope.booking.date = $("#basic_example_1").val();				
+				$("#confirm-popup").modal();
+				$scope.$apply();
+			} else {
+				alert("Unable to find the distance via road.");
+			}
+		});	
+	}
+	
+	$scope.confirmRide = function(){
+		$scope.bookingData.userId = USER_ID;
+		$scope.bookingData.from = $scope.booking.from.formatted_address;
+		$scope.bookingData.to = $scope.booking.to.formatted_address;
+		$scope.bookingData.kms = $scope.booking.distance;
+		$scope.bookingData.date = $scope.booking.date;
+		
+		$http.post( API_URL + 'booking.php',{ data: $scope.bookingData }).success( function( response ){
+			if( response.code == 200 ){				
+				$("#confirm-popup").modal('hide');
+				$("#success-popup").modal();
+				$(".form-success").addClass('hide');
+				$scope.rideStatus();
+			}
+		});
+	}
+	
+	$scope.rideStatus = function(){
+		$scope.bookingData.userId = USER_ID;
+		$http.post( API_URL + 'rideStatus.php',{ data: $scope.bookingData }).success( function( response ){
+			if( response.code == 400 ){
+				$("#pending-popup").modal();
+				$(".form-pending").removeClass('hide');
+			}else if( response.code == 200 ){
+				$(".form-success").removeClass('hide');
+			}
+		});
+	}
+	
+	$scope.rideStatus();
+});
 
-      function initAutocomplete() {
-        // Create the autocomplete object, restricting the search to geographical
-        // location types.
-        autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),{types: ['geocode']});
-        //autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocompleteTo')),{types: ['geocode']});
+/* PROFILE CONTROLLER */
+app.controller( 'profileCtrl', function( $scope, $log, $http, Constants, toastr ){
+	var API_URL = Constants.apiUrl();
+	var USER_ID = Constants.userId();
+	
+	$scope.getProfile = function(){
+		$http.post( API_URL + 'profile.php',{ data: USER_ID}).success( function( response ){
+			$scope.profile = response.data;
+		});
+	}
+	
+	$scope.profileFormsubmit = function( profile ){
+		profile.userId = USER_ID;
+		$log.log( profile );
+		$http.post( API_URL + 'updateProfile.php',{ data: profile}).success( function( response ){
+			if( response.code == 200 ){
+				toastr.success( "Successfully updated!" );
+				$scope.getProfile();
+			}else{
+				toastr.error( "Something went wrong!" );
+			}
+		});
+	}
+	
+	$scope.getProfile();
+});
 
-        // When the user selects an address from the dropdown, populate the address
-        // fields in the form.
-        autocomplete.addListener('place_changed', fillInAddress);
-      }
-
-      function fillInAddress() {
-        
-      }
-
-      // Bias the autocomplete object to the user's geographical location,
-      // as supplied by the browser's 'navigator.geolocation' object.
-      function geolocate() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var geolocation = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            var circle = new google.maps.Circle({
-              center: geolocation,
-              radius: position.coords.accuracy
-            });
-            autocomplete.setBounds(circle.getBounds());
-          });
-        }
-      } */
-	  
-	  function initAutocomplete() {
-  const googleComponents = [
-    { googleComponent: 'sublocality_level_1', id: 'city-address-field' },
-    { googleComponent: 'locality', id: 'city-address-field' },
-    { googleComponent: 'administrative_area_level_1', id: 'state-address-field' },
-    { googleComponent: 'postal_code', id: 'postal-code-address-field' },
-  ];
-  const autocompleteFormField = document.getElementById('autocomplete');
-  const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
-    types: ['address'],
-    componentRestrictions: ['in'],
-  });
-  google.maps.event.clearInstanceListeners(autocompleteFormField);
-  google.maps.event.addListener(autocomplete, 'place_changed', function(){
-    const place = autocomplete.getPlace();
-	console.log(place)
-    /* autocompleteFormField.value = place.name;
-    for (const component in googleComponents) {
-      const addressComponents = place.address_components;
-      addressComponents.forEach( addressComponent => populateFormElements(addressComponent, googleComponents[component]));
-    } */
-  });
-}
-
-	 initAutocomplete(); 
+/* RIDES CONTROLLER */
+app.controller( 'ridesCtrl', function( $scope, $log, $http, Constants, toastr ){
+	var API_URL = Constants.apiUrl();
+	var USER_ID = Constants.userId();
+	
+	$scope.getRides = function(){
+		$http.post( API_URL + 'getRides.php',{ data: USER_ID}).success( function( response ){
+			$scope.ridesList = response.data;
+		});
+	}
+	
+	$scope.getRides();
 });
